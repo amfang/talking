e197d2c3d44c92e20b365ee4bae7f9af2d9e8c6f
amfang <JackyFang@JackydeMacBook-Air.local>
2016-12-13 23:20:04 +0800

    c e add updateMessage function

--- app.js
+++ app.js
@@ -84,16 +84,42 @@ app.post ( '/api/message', function (req, res) {
       console.error ( JSON.stringify ( err ) );
       return res.status ( err.code || 500 ).json ( err );
     }
-    if ( logs ) {
-      //If the logs db is set, then we want to record all input and responses
-      var id = uuid.v4 ();
-      logs.insert ( {'_id': id, 'request': payload, 'response': data, 'time': new Date ()}, function (err, data) {
+    console.log('request : payload; response: '+ data +'; time: '+new Date ());
+    return res.json(updateMessage(payload, data));
+  });
+});
 
-      } );
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Conversation service
+ * @param  {Object} response The response from the Conversation service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
     }
-    return res.json ( data );
-  } );
-} );
+  }
+  response.output.text = responseText;
+  return response;
+}
 
 
 app.use ( '/api/speech-to-text/', require ( './speech/stt-token.js' ) );
